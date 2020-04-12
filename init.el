7a7ff842ba9702221c70949fbfd3bcc02544dffa
pranav <pranavkelkar@protonmail.com>
2020-04-12 13:20:12 +0530

    Inital commit

---
+++ init.el
@@ -0,0 +1,71 @@
+;; Do not show the startup screen.
+(setq inhibit-startup-message t)
+
+;; Disable tool bar, menu bar, scroll bar.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Highlight current line.
+(global-hl-line-mode t)
+
+;; Use `command` as `meta` in macOS.
+;;(setq mac-command-modifier 'meta)
+
+;; Do not use `init.el` for `custom-*` code - use `custom-file.el`.
+(setq custom-file "~/.emacs.d/custom-file.el")
+
+;; Assuming that the code in custom-file is execute before the code
+;; ahead of this line is not a safe assumption. So load this file
+;; proactively.
+(load-file custom-file)
+
+;; Require and initialize `package`.
+(require 'package)
+(package-initialize)
+
+;; Add `melpa` to `package-archives`.
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;; Additional packages and their configurations
+
+(use-package ewal-spacemacs-themes
+  :disabled t
+  :ensure t
+  :config
+  (setq-default spacemacs-theme-comment-bg nil
+                spacemacs-theme-comment-italic t)
+  (load-theme 'spacemacs-dark t)
+  :delight)
+
+(use-package nord-theme
+  :ensure t
+  :config
+  (load-theme 'nord t)
+
+  :delight)
+
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-center-theme)
+  :delight)
+
+(use-package company
+  :ensure t
+  ;; Navigate in completion minibuffer with `C-n` and `C-p`.
+  :bind (:map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  ;; Provide instant autocompletion.
+  (setq company-idle-delay 0.3)
+
+  ;; Use company mode everywhere.
+  (global-company-mode t))
